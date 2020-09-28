03652663aeb47db8e1b942b86cfdec6cd45ce237
radualexvornicu <radualex.vornicu@gmail.com>
2020-09-28 16:59:03 +0200

    console.log's removed from build stage

--- front-end/js/furnitureSort.js
+++ front-end/js/furnitureSort.js
@@ -3,7 +3,6 @@ const serverUrl = "http://localhost:3000/api/furniture/";
 // Denifir la class
 class furnitureSort {
     constructor() {
-        console.log('all good here');
         this.furnitures = null;
         this.furniture = null;
         this.basketBack = null;
@@ -17,7 +16,6 @@ class furnitureSort {
     // Appel de l'API => promesse
     getAllFurniture = async function () {
         let response = await fetch(serverUrl)
-
         if (response.ok) {
             return response.json()
         } else {
@@ -64,13 +62,10 @@ class furnitureSort {
     // Utilise la réponse pour remplir la carte produit
     getOneFurniture() {
         let id = location.search.substring(4);
-        console.log(id);
         fetch(serverUrl + '/' + id)
             .then(response => response.json())
             .then((response) => {
-                console.log(this.furniture);
                 this.furniture = response;
-                console.log(this.furniture);
                 document.getElementById('name').textContent = this.furniture.name;
                 document.getElementById('image').setAttribute('src', this.furniture.imageUrl);
                 document.getElementById('image').setAttribute('alt', 'preview picture of' + this.furniture.name);
@@ -102,20 +97,15 @@ class furnitureSort {
         }
         // Si no => adjoute la furniture aux tablou
         if (ok === 1) {
-            console.log(furnitureBasket);
             basketBag.push(furnitureBasket);
-            console.log(basketBag);
         }
         // Retourner le tablou avec tout les produits
         return basketBag;
     }
     // Aux click event ('Metre aux panier')
     addFurniture(event) {
-        // console.log du furniture qui va etre adjoute aux panier
-        console.log(this.furniture);
         // Retrouve le panier du localStorage
         let storageBasketBack = localStorage.getItem('storageBasket');
-        console.log(storageBasketBack);
         // Si le panier existe deja, verifier et adjoute le produit
         if (storageBasketBack) {
             let basketBag = JSON.parse(storageBasketBack);
@@ -132,7 +122,6 @@ class furnitureSort {
                 this.checkIfDuplicat(basketBag, furnitureBasket);
                 // Adjoute le tablou avec tous les produid et le neuveaux produit selecte
                 let storageBasketGo = JSON.stringify(basketBag);
-                console.log(storageBasketGo);
                 localStorage.setItem('storageBasket', storageBasketGo);
             }
             // Si le panier est vide/il n'exist pas dans le localStorage
@@ -146,12 +135,8 @@ class furnitureSort {
                 count: 1,
                 url: this.furniture.imageUrl,
             }
-            console.log(furnitureBasket);
             tableBasket.push(furnitureBasket);
-            console.log(tableBasket);
-
             let storageBasketGo = JSON.stringify(tableBasket);
-            console.log(storageBasketGo);
             localStorage.setItem('storageBasket', storageBasketGo);
         }
         // Apell aux fonction qui compte le nombre total des produits qui sont dans le panier
@@ -160,7 +145,6 @@ class furnitureSort {
     // Realiser un EventListener ('click) pour le button 'Mettre dans le panier' 
     putInStorage() {
         document.getElementById('basket').addEventListener('click', (event) => {
-            console.log(this);
             // Apell aux fonction qui va adjoute le produit aux panier
             this.addFurniture(event);
             alert("Vous avez ajouté ce produit dans votre panier!'\n'Vous pouvez retourner à la liste des produits!'\n'Ou verifier votre panier!")
@@ -171,10 +155,7 @@ class furnitureSort {
     // Name, count et price + button de souprime le produit
     createItemsInBasket(basketBag) {
         this.basketBack = basketBag;
-        console.log(this.basketBack.length);
-        console.log(this.basketBack);
         for (let i = 0; i < this.basketBack.length; i++) {
-            console.log('inner for message ok');
             // Cree un 'div' HTLM dans le fichier panier.html qui contient le Name, count et price + button de souprime le produit
             let item = document.createElement("div");
             // Cree un 'img' avec la image du produit
@@ -202,7 +183,6 @@ class furnitureSort {
             cancel.setAttribute('class', 'close text-success');
             cancel.textContent = "x"
             cancel.addEventListener('click', (event) => {
-                console.log('this furniture is removed');
                 this.removeFurniture(i);
             })
             let hr = document.createElement("hr");
@@ -223,7 +203,6 @@ class furnitureSort {
             total += basketBag.count * basketBag.price / 100;
         })
         this.priceFinal = total;
-        console.log(this.priceFinal);
         let priceTotal = document.createElement("div");
         priceTotal.setAttribute('class', 'p-2 w-50 m-0');
         priceTotal.textContent = 'Prix total : ' + this.priceFinal + ' Euro';
@@ -240,7 +219,6 @@ class furnitureSort {
             localStorage.clear();
             this.getFromStorage();
             this.getBasketCount();
-            console.log(clearBasket);
             clearBasket.remove();
             document.getElementById('basketInner').remove();
         })
@@ -257,7 +235,6 @@ class furnitureSort {
     // ou qui va indique que le panier est vide ou que il a devenu vide    
     basketTextCheck(basketBag) {
         if (basketBag.length == 0) {
-            console.log(basketBag.length);
             this.createPanierVide();
             if (document.getElementById('clearBasket')) {
                 document.getElementById('clearBasket').remove();
@@ -276,9 +253,7 @@ class furnitureSort {
     // et affiche tous les produits du panier
     getFromStorage() {
         let storageBasketBack = localStorage.getItem('storageBasket');
-        console.log(storageBasketBack);
         let basketBag = JSON.parse(storageBasketBack);
-        console.log(basketBag);
         if (storageBasketBack && storageBasketBack.length > 0) {
             let basketBag = JSON.parse(storageBasketBack);
             if (Array.isArray(basketBag)) {
@@ -304,7 +279,6 @@ class furnitureSort {
             this.createPanierVide();
         }
         document.getElementById('sendPost').addEventListener('click', (event) => {
-            console.log('we have a click');
             this.checkEmptyInput(event);
         })
     }
@@ -312,9 +286,7 @@ class furnitureSort {
     // l'appel est realise quand la quantite se modifie
     getBasketCount() {
         let storageBasketBack = localStorage.getItem('storageBasket');
-        console.log(storageBasketBack);
         let basketBag = JSON.parse(storageBasketBack);
-        console.log(basketBag);
         if (storageBasketBack) {
             let count = 0;
             for (let j = 0; j < basketBag.length; j++) {
@@ -327,26 +299,18 @@ class furnitureSort {
     }
     // La fonction qui realise la elimination d'un produit ou de reduir ca quantite
     removeFurniture(i) {
-        console.log('it has been canceled');
         // Si la quantite est plus de 1, reduir la quantite
         if (this.basketBack[i].count > 1) {
-            console.log('count reduced');
             this.basketBack[i].count -= 1;
-            console.log(this.basketBack[i].count);
             let storageBasketBack = JSON.stringify(this.basketBack);
             localStorage.clear();
             localStorage.setItem('storageBasket', storageBasketBack);
-            console.log('localStorage has been updated? after the count reduce?');
             // Si la quantite est unique, eliminer totalement le produit et son contenu HTLM
         } else {
             this.basketBack.splice(i, 1);
-            console.log(this.basketBack);
             let storageBasketBack = JSON.stringify(this.basketBack);
-            console.log(storageBasketBack);
             localStorage.clear();
-            console.log('localStorage empty');
             localStorage.setItem('storageBasket', storageBasketBack);
-            console.log('localStorage has been updated?');
             // Apre la elimination, recreer la liste des produits
             this.getFromStorage();
         }
@@ -359,9 +323,7 @@ class furnitureSort {
     checkEmptyInput(event) {
         event.preventDefault();
         let storageBasketBack = localStorage.getItem('storageBasket');
-        console.log(storageBasketBack);
         let basketBack = JSON.parse(storageBasketBack);
-        console.log(basketBack);
         // Si le panier est pas vide et sa longueur supérieur à 0 sinon, alert panier vide
         if (basketBack != null && basketBack.length > 0) {
             var tabId = new Array('formName', 'formPrename', 'formMail', 'formAdresse', 'formCity');
@@ -378,7 +340,6 @@ class furnitureSort {
             if (mes != '') {
                 alert('ERREUR :' + br + br + 'Il manque :' + mes);
             } else {
-                console.log('no empty fields');
                 this.controlBasket(event);
             }
         } else {
@@ -388,7 +349,6 @@ class furnitureSort {
     // Vérifie les inputs du formulaire
     controlBasket(event) {
         event.preventDefault();
-        console.log('Go Go control !');
         let regex = /[~`!#$%\^&*+=\-\[\]\';,/{}|\":<>\?0123456789 ]/;
         let regexCity = /[~`!#$%\^&*+=[\]\';,/{}|\":<>\?0123456789]/;
         let regexAddress = /[~`!#$%\^&*+=[\]\';/{}|\":<>\?]/;
@@ -399,53 +359,48 @@ class furnitureSort {
         let formCity = document.getElementById('formCity').value;
         let formMail = document.getElementById('formMail').value;
         let br = '\n', mes = '';
-        if(regex.test(formNom)==true){
+        if (regex.test(formNom) == true) {
             console.log('nom invalide');
             mes = mes + br + ' - Nom invalide;';
-        }else{
+        } else {
             console.log('nom ok')
         }
-        if(regex.test(formPrename)==true){
+        if (regex.test(formPrename) == true) {
             console.log('Prénom invalide');
             mes = mes + br + ' - Prénom invalide;';
-        }else{
+        } else {
             console.log('prénom ok')
         }
-        if(regexCity.test(formCity)==true){
+        if (regexCity.test(formCity) == true) {
             console.log('Ville invalide');
             mes = mes + br + ' - Nom de ville invalide;';
-        }else{
+        } else {
             console.log('ville ok')
         }
-        if(regexMail.test(formMail)==false){
+        if (regexMail.test(formMail) == false) {
             console.log('mail invalide');
             mes = mes + br + ' - Mail invalide;';
-        }else{
+        } else {
             console.log('mail ok')
         }
-        if(regexAddress.test(formAdresse)==true){
+        if (regexAddress.test(formAdresse) == true) {
             console.log('Address invalide');
             mes = mes + br + ' - Address invalide;';
-        }else{
+        } else {
             console.log('address ok')
         }
-        if(regex.test(formNom)==false && regex.test(formPrename)==false && regex.test(formCity)==false && regexMail.test(formMail)==true && regexAddress.test(formAdresse)==false){
-            console.log('On peut POST !')           
+        if (regex.test(formNom) == false && regex.test(formPrename) == false && regex.test(formCity) == false && regexMail.test(formMail) == true && regexAddress.test(formAdresse) == false) {
             this.postFurniture(event); // passer à la page confirmation seulement après que postTeddy soit ok
-        }else{
+        } else {
             //affiche le message d'erreur
-            console.log('il reste un problème');
             alert('ERREUR :' + br + br + mes);
         }
     };
     // Envoie la requête post de la commande avec le tableau d'items et l'objet contact
     postFurniture() {
         let arrayProductsT = [];
-        console.log(this.basketBack)
         this.basketBack.forEach(function (basketBag) {
-            console.log('table demanted to be fill')
             arrayProductsT.push(basketBag.id);
-            console.log(arrayProductsT);
         });
         this.arrayProducts = arrayProductsT;
         sessionStorage.clear();
@@ -465,7 +420,6 @@ class furnitureSort {
         }
         // Définition de la requête POST au serveur
         // et des actions: vide le panier (localStorage), met la valeur du montant total du panier dans le localStorage
-        console.log(data);
         fetch('http://localhost:3000/api/furniture/order', {
             method: 'post',
             headers: {
@@ -478,8 +432,6 @@ class furnitureSort {
                 return res.json();
             }
         }).then(data => {
-            console.log(data.contact);
-            console.log(data.orderId);
             sessionStorage.setItem('order', data.orderId);
             sessionStorage.setItem('total', JSON.stringify(this.priceFinal));
             // Si le panier est vide la commande n'est pas envoyée au serveur
@@ -497,11 +449,8 @@ class furnitureSort {
     };
     // Affiche le message de confirmation à partir du sessionStorage
     showOrder() {
-        console.log('let\'s the show begin !');
         let orderFinal = sessionStorage.getItem('order');
-        console.log(orderFinal);
         let priceShow = sessionStorage.getItem('total');
-        console.log(priceShow);
         let name = sessionStorage.getItem('name');
         let merciMess = document.getElementById('name');
         let order = document.getElementById('order');
